1ac46680e4a71b01bba475e8e084be0a2b5e7bf1
James Liao <james.liao@tenmax.io>
2021-07-20 12:15:13 +0800

    can download sample pdf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $faker = Faker::create();
+
+    	foreach (range(1,10) as $index) {
+            DB::table('employees')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'phone_number' => $faker->phoneNumber,
+                'dob' => $faker->date($format = 'D-m-y', $max = '2000',$min = '1990')
+            ]);
+        }
     }
 }
